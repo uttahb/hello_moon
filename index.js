880f8726481ff880c1b73ebef5213a6fc5938d09
Jishnu <jishnu.nb@neoito.com>
2022-10-03 12:29:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const hello_moon = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello hello_moon`}))
+  res.end()
+  
+}
+
+export default hello_moon
